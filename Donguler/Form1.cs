72e0f0d5e498b47af4eed7a74bebb28c0c55f6ed
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-26 16:45:13 +0300

    Ekrana birden 10 a kadar olan sayıları yazdrıan program

--- Donguler/Form1.cs
+++ Donguler/Form1.cs
@@ -19,9 +19,9 @@ namespace Donguler
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i < 11; i++)
             {
-                listBox1.Items.Add(i + " Merhaba Ankara ");
+                listBox1.Items.Add(i);
             }
             
 
