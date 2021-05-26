7f91aacf8f8f3cf36b850e5a7ffae688e61ebbad
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-26 16:41:42 +0300

    Listboxa for döngüsü ile 10 defa merhaba ankara yazdrıan progam

--- Donguler/Form1.cs
+++ Donguler/Form1.cs
@@ -16,5 +16,16 @@ namespace Donguler
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add("Merhaba Ankara");
+            }
+            
+
+
+        }
     }
 }
