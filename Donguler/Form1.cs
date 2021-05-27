0145b974e3b280da875097aa5c95af45fc82b4bc
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-27 10:22:00 +0300

    1 ile 100 arasında 5 ve 7 e bölünen sayıları listeyen program

--- Donguler/Form1.cs
+++ Donguler/Form1.cs
@@ -50,9 +50,9 @@ namespace Donguler
 
         private void button4_Click(object sender, EventArgs e)
         {
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 100; i++)
             {
-                if(i% 3 == 0)
+                if(i% 5 == 0 && i% 7 ==0 )
                         {
                     listBox2.Items.Add(i);     
                         }
